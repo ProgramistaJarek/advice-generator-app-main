48d37265b896f9c4c76ff10befe2f63f4b2cea3f
Jarek <dojarka0@gmail.com>
2022-03-13 12:43:46 +0100

    simple advice app with api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState([]);
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  const requset = `https://api.adviceslip.com/advice`;
+  const getAdvice = async () => {
+    const response = await fetch(requset);
+    const data = await response.json();
+    setAdvice(data.slip);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="advice-component">
+      <h1 className="advice">Advice #{advice.id}</h1>
+      <p className="qoute">&#8220;{advice.advice}&#8221;</p>
+      <div className="test">
+        <span>
+          <img
+            className="qoute-icon"
+            src="\images\pattern-divider-mobile.svg"
+            alt="line"
+          />
+        </span>
+      </div>
+      <button className="btn " type="submit" onClick={getAdvice}>
+        <img src="\images\icon-dice.svg" alt="roll" />
+      </button>
     </div>
   );
 }
